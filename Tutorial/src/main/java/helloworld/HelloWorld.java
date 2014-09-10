7fb3c76cedc534ebcc68501b6bb4b90c87294665
subodh_nayak <subodh.nayak@techmahindra.com>
2014-09-10 08:24:27 +0530

    Initial commit for Tutorial.

---
+++ Tutorial/src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author subodh_nayak
+ *
+ */
+public class HelloWorld {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+}
